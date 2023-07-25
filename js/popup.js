0fd20ccd591176a207c9f1a2104efcd2f045a130
Евгений Котенев <evgenijkotenev@MacBook-Pro-Evgenij.local>
2023-07-25 15:50:14 +0300

    update

--- js/popup.js
+++ js/popup.js
@@ -4,19 +4,19 @@ const BODY_FIXED_CLASSNAME = 'body_fixed'
 const bodyNode = document.querySelector('body')
 const popupNode = document.querySelector('.js-popup')
 const btnOpenNode = document.querySelector('.js-btn')
-// const popupContentNode = document.querySelector('.js-popup__content')
+const popupContentNode = document.querySelector('.js-popup__content')
 const btnCloseNode = document.querySelector('.js-popup__close-btn')
 
 btnOpenNode.addEventListener('click', togglePopup)
 btnCloseNode.addEventListener('click', togglePopup)
 
-// popupNode.addEventListener('click', event => {
-// 	const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
+popupNode.addEventListener('click', event => {
+	const isClickOutsideContent = !event.composedPath().includes(popupContentNode)
 
-// 	if (isClickOutsideContent) {
-// 		togglePopup()
-// 	}
-// })
+	if (isClickOutsideContent) {
+		togglePopup()
+	}
+})
 
 function togglePopup() {
 	popupNode.classList.toggle(POPUP_OPENED_CLASSNAME)
